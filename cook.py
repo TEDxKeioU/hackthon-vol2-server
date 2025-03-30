5803418cc19e3d5122371193bf28c5f3531b3ce0
xts777 <126665086+xts777@users.noreply.github.com>
2025-03-30 22:11:07 +0900

    refactor & add api_key variables into code (API_KEY is hidden in the env

--- cook.py
+++ cook.py
@@ -1,17 +1,15 @@
-from dotenv import load_dotenv 
-from langchain.llms import OpenAI 
-from langchain.chat_models import ChatOpenAI 
+from dotenv import load_dotenv
+from langchain_openai import OpenAI, ChatOpenAI 
 from langchain.schema import HumanMessage 
 import os 
 
 load_dotenv() 
-api_key = os.getenv('OPEN_API_KEY') 
-print(api_key) 
+api_key = os.getenv('OPEN_API_KEY')
 
-llm = OpenAI(temperature=0.9)
+# llm = OpenAI(openai_api_key=api_key, temperature=0.9)
 
-chat_model = ChatOpenAI(model_name="gpt-4o", temperature=0) 
+chat_model = ChatOpenAI(openai_api_key=api_key, model_name="gpt-4o", temperature=0) 
 ingredients = input() 
 messages = [HumanMessage(content=f"{ingredients}を使ったレシピを提案してください")] 
-response = chat_model(messages) 
+response = chat_model.invoke(messages)
 print(response.content)
